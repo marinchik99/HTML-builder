e4aa7a0cc1e31326d7c1c8da77ca000b761fb908
marinchik99 <marina_1999@tut.by>
2021-11-07 12:09:05 +0300

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+let file = path.join(__dirname, 'files');
+let copy = path.join(__dirname, 'files-copy');
+
+fs.mkdir(copy, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(copy, (err, files) => {
+    if (err) throw err;
+  
+    for (let i=0; i<files.length; i++){
+      let copyF = path.join(copy, files[i]);
+  
+      fs.unlink(copyF, (err) => {
+        if (err) throw err;
+      });
+    };
+  });  
+
+  fs.readdir(file, (err, files) => {
+    if (err) throw err;
+  
+    for (let i=0; i<files.length; i++){
+      let fileP = path.join(file,files[i]);    
+      let copyF = path.join(copy, files[i]);
+  
+      fs.copyFile(fileP, copyF, (err) => {
+        if (err) throw err;
+      });
+    };
+  });
+
 
